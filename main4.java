2dc3d9bd51cfa973c1c3afd2c8c0fbd543dc1f8c
ismetakpinar <104082595+ismetakpinar@users.noreply.github.com>
2022-10-27 17:13:41 +0300

    Update main4.java

--- main4.java
+++ main4.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class main4 {
+import java.util.Scanner;
+
+public class main4 {
+    public static void main(String[] args) {
+        double gidilenyol;
+        double kmbasiucret= 2.20;
+        double tutar ;
+        int acilisucreti = 10;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Gidilen yolu KM cinsinden giriniz :");
+        gidilenyol = input.nextDouble();
+
+        tutar = gidilenyol*kmbasiucret + acilisucreti;
+
+        tutar = tutar <20 ? 20 : tutar;
+        System.out.println("Ödenecek ücret : " + tutar);
+    }
+
+
 }
